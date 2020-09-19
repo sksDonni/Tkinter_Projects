d8a5a77b38b79da8aee1b51d33b002806528726c
suchith Krishna S Donni <sksdonni18@gmail.com>
2020-09-19 12:28:27 +0530

    improvised the imageviewer.py

--- imageEditor/imageviewer.py
+++ imageEditor/imageviewer.py
@@ -11,22 +11,65 @@ def get_images(path):
         img = Image.open(path + image)
         img = img.resize((500, 500), Image.ANTIALIAS)
         load_images.append(img)
-    print(len(load_images))
 
 
 def next_image(image_number):
     global button_forward
+    global button_back
     global my_title
 
-    if len(load_images) >= image_number:
-        my_title.pack_forget()
-        img = ImageTk.PhotoImage(load_images[image_number - 1])
-        my_title = Label(image=img)
-        my_title.image = img
-        my_title.pack()
-        button_forward.pack_forget()
+    print(image_number)
+    my_title.grid_forget()
+    img = ImageTk.PhotoImage(load_images[image_number])
+    my_title = Label(image=img)
+    my_title.image = img
+    my_title.grid(row=0, column=0, columnspan=3)
+    button_back.grid_forget()
+    button_forward.grid_forget()
+    if image_number >= len(load_images) - 1:
+        button_forward = Button(text="Next Image", state=DISABLED)
+        button_forward.grid(row=1, column=2)
+        print("Disabled")
+    else:
         button_forward = Button(text="Next Image", command=lambda: next_image(image_number + 1))
-        button_forward.pack()
+        button_forward.grid(row=1, column=2)
+
+    if image_number <= 0:
+        button_back = Button(text="Previous Image", state=DISABLED)
+        button_back.grid(row=1, column=0)
+    else:
+        button_back = Button(text="Previous Image", command=lambda: prev_image(image_number - 1))
+        button_back.grid(row=1, column=0)
+
+
+def prev_image(image_number):
+
+    global my_title
+    global button_forward
+    global button_back
+
+    print(image_number)
+    my_title.grid_forget()
+    img = ImageTk.PhotoImage(load_images[image_number])
+    my_title = Label(image=img)
+    my_title.image = img
+    my_title.grid(row=0, column=0, columnspan=3)
+    button_forward.grid_forget()
+    button_back.grid_forget()
+
+    if image_number >= len(load_images) - 1:
+        button_forward = Button(text="Next Image", state=DISABLED)
+        button_forward.grid(row=1, column=2)
+    else:
+        button_forward = Button(text="Next Image", command=lambda: next_image(image_number + 1))
+        button_forward.grid(row=1, column=2)
+
+    if image_number <= 0:
+        button_back = Button(text="previous image", state=DISABLED)
+        button_back.grid(row=1, column=0)
+    else:
+        button_back = Button(text="Previous image", command=lambda: prev_image(image_number - 1))
+        button_back.grid(row=1, column=0)
 
 
 if __name__ == '__main__':
@@ -42,14 +85,19 @@ if __name__ == '__main__':
     my_image = Label
     get_images(path)
 
+    button_forward = Button(text="Next Image", command=lambda: next_image(image_number + 1))
+    button_forward.grid(row=1, column=2)
+    button_back = Button(text="Previous Image", command=lambda: prev_image(image_number - 1))
+    button_back.grid(row=1, column=0)
+
     img = ImageTk.PhotoImage(load_images[0])
     global my_title
     my_title = Label(image=img)
-    my_title.pack()
+    my_title.grid(row=0, column=0, columnspan=3)
     image_number = 0
-    button_forward = Button(text="Next Image", command=lambda: next_image(image_number))
-    button_forward.pack()
 
+    prev_image(0)
+    next_image(0)
     root.mainloop()
 
 
